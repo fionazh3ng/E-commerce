aaf87c4dad3ca96ddc352ee7959752a9c4e4c56f
Fiona Zheng <fionazh3ng@fionas-mbp.mynetworksettings.com>
2024-02-12 14:29:10 -0500

    finished users and products page

--- E-commerce/backend/api/index.js
+++ E-commerce/backend/api/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use("/orders", require("./orders"));
 router.use("/users", require("./users"));
+router.use("/products", require("./products"));
 
 module.exports = router;
