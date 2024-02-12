d2813a9a39b6ecbeed39a4b774335bfd70ac4fa1
Qiao Gan Chen <qiaoganchen@gmail.com>
2024-02-12 17:04:21 -0500

    cart

--- E-commerce/backend/api/index.js
+++ E-commerce/backend/api/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/orders", require("./orders"));
-router.use("/users", require("./users"));
-router.use("/products", require("./products"));
+router.use("/cart", require("./cart"));
 
 module.exports = router;
