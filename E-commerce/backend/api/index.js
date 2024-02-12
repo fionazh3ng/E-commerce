17ad94aba422f1bf2868d06c5a4c46b03c0793a7
Fiona Zheng <128185933+fionazh3ng@users.noreply.github.com>
2024-02-12 13:02:24 -0500

    Delete E-commerce directory

--- E-commerce/backend/api/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use("/auth"), require("../auth/index");
-router.use("/users"), require("./users");
-
-module.exports = router;
