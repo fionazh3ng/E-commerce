ec20a045cf05a938e5d6a52da97a3623a2eded9f
qiaoganchen22 <Qiaoganchen@gmail.com>
2024-02-12 11:47:10 -0500

    Update index.js

--- E-commerce/backend/api/index.js
+++ E-commerce/backend/api/index.js
@@ -2,6 +2,5 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/orders", require("./orders"));
-//router.use("/orderdetails", require("./orderDetails"));
 
 module.exports = router;
