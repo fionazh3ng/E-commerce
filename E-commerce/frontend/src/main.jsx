b3948dfee3591316744e4d824553782b3e157aaa
Qiao Gan Chen <qiaoganchen@gmail.com>
2024-02-14 16:35:29 -0500

    added routing

--- E-commerce/frontend/src/main.jsx
+++ E-commerce/frontend/src/main.jsx
@@ -4,11 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import {store} from "../api/store.js"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store ={store}>
   <React.StrictMode>
-    <App />
+  <Router>
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+        </Routes>
+      </Router>
   </React.StrictMode>
   </Provider>
 )
